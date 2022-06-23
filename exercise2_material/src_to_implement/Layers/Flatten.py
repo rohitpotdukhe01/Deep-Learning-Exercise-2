81161722ccaa6eb7281b3dbf640626d10914e09f
Chitra23Ahuja <105649388+Chitra23Ahuja@users.noreply.github.com>
2022-06-23 13:36:27 +0200

    Update Flatten.py

--- exercise2_material/src_to_implement/Layers/Flatten.py
+++ exercise2_material/src_to_implement/Layers/Flatten.py
@@ -1,16 +1,23 @@
 import numpy as np
+from .Base import BaseLayer
 
-class Flatten():
+
+class Flatten(BaseLayer):
 
     def __init__(self):
+        super().__init__()
         self.output = None
         self.trainable = False
+        self.forward_output = None
+        self.backward_output = None
 
     def forward(self, input_tensor):
-        self.output = np.arange(input_tensor).reshape(input_tensor)
+        self.forward_output = np.shape(input_tensor)
+        self.output = input_tensor.reshape(self.forward_output[0],-1)
         return self.output.copy()
 
-    def backward(self,error_tensor):
-        self.output = np.reshape(np.array(error_tensor))
+    def backward(self, error_tensor):
+        self.output = error_tensor.reshape(self.forward_output)
         return self.output.copy()
+
 
