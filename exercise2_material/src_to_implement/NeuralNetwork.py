a67ae5d3c6b6faa64f8a6080fe749766139d64d4
Chitra23Ahuja <105649388+Chitra23Ahuja@users.noreply.github.com>
2022-06-23 17:59:38 +0200

    Create NeuralNetwork.py

---
+++ exercise2_material/src_to_implement/NeuralNetwork.py
@@ -0,0 +1,44 @@
+import copy as cp
+
+class NeuralNetwork():
+    def __init__(self, optimizer, weight_initializers, bias_initializers):
+        self.optimizer = optimizer
+        self.loss = list()
+        self.layers = list()
+        self.data_layer = None
+        self.loss_layer = None
+        self.next_input = None
+        self.labels_input = None
+        self.weight_initializers = weight_initializers
+        self.bais_initializers = bias_initializers
+        self.output = None
+
+    def forward(self):
+        self.next_input, self.labels_input = self.data_layer.next()
+        input_copy = self.next_input.copy()
+        for layer in self.layers:
+            input_copy = layer.forward(input_copy)
+        self.output = self.loss_layer.forward(input_copy, self.labels_input)
+        return self.output
+
+    def backward(self):
+        loss_gradiant = self.loss_layer.backward(self.labels_input)
+        for layer in self.layers[::-1]:
+            loss_gradiant = layer.backward(loss_gradiant)
+
+    def append_layer(self, layer):
+        if layer.trainable:
+            layer.optimizer = cp.deepcopy(self.optimizer)
+            layer.initializers(self.weight_initializers,self.bais_initializers)
+        self.layers.append(layer)
+
+    def train(self, iterations):
+        for i in range(iterations):
+            self.loss.append(self.forward())
+            self.backward()
+
+    def test(self, input_tensor):
+        inp = input_tensor
+        for layer in self.layers:
+            inp = layer.forward(inp)
+        return inp
