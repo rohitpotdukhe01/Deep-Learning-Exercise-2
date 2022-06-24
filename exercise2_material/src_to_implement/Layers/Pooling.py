04113e154da6cb1a0e568973595898481998d888
Rohit Potdukhe <89935999+rohitpotdukhe01@users.noreply.github.com>
2022-06-24 07:19:48 +0200

    Update Pooling.py

--- exercise2_material/src_to_implement/Layers/Pooling.py
+++ exercise2_material/src_to_implement/Layers/Pooling.py
@@ -3,17 +3,17 @@ from .Base import BaseLayer
 
 
 class Pooling(BaseLayer):
-    def __init__(self, stride_shape=(2, 2), pooling_shape=(2, 2)):
+    def __init__(self, stride_shape, pooling_shape):
         super().__init__()
         self.back_input = None
-        self.stride = stride_shape
+        self.stride_shape = stride_shape
         self.pooling_shape = pooling_shape
 
     def forward(self, input_tensor):
         self.back_input = input_tensor
         batch_size, num_channels, input_height, input_width = input_tensor.shape
-        output_height = int((input_height - self.pooling_shape[1]) / self.stride[0]) + 1
-        output_width = int((input_width - self.pooling_shape[0]) / self.stride[1]) + 1
+        output_height = int((input_height - self.pooling_shape[1]) / self.stride_shape[0]) + 1
+        output_width = int((input_width - self.pooling_shape[0]) / self.stride_shape[1]) + 1
 
         down_sampling = np.zeros((batch_size, num_channels, output_height, output_width))
         for i in range(batch_size):
@@ -25,9 +25,9 @@ class Pooling(BaseLayer):
                         patch = input_tensor[i, j, current_y:current_y + self.pooling_shape[1],
                                 current_x:current_x + self.pooling_shape[0]]
                         down_sampling[i, j, output_y, output_x] = np.max(patch)
-                        current_x += self.stride[1]
+                        current_x += self.stride_shape[1]
                         output_x += 1
-                    current_y += self.stride[0]
+                    current_y += self.stride_shape[0]
                     output_y += 1
 
         return down_sampling
@@ -46,9 +46,9 @@ class Pooling(BaseLayer):
                                 temp_x:temp_x + self.pooling_shape[0]]
                         (x, y) = np.unravel_index(np.nanargmax(patch), patch.shape)
                         output[i, j, temp_y + x, temp_x + y] += error_tensor[i, j, output_y, output_x]
-                        temp_x += self.stride[1]
+                        temp_x += self.stride_shape[1]
                         output_x += 1
-                    temp_y += self.stride[0]
+                    temp_y += self.stride_shape[0]
                     output_y += 1
 
         return output
